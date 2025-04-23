fc9d314d4eb6fe51b2738411ec8bafb145401727
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-23 21:02:54 +0200

    edit Main Activity

--- app/src/main/java/com/example/substracker/MainActivity.kt
+++ app/src/main/java/com/example/substracker/MainActivity.kt
@@ -4,9 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
